15bdbb49add60b4e4e677d137bded6d48bf4f77c
Thiago Morette <thiagomorette@gmail.com>
2019-08-02 19:59:45 -0300

    desafio da aula 047 concluído

---
+++ jogo-da-velha/src/components/background/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css';
+import MenuAbout from '../menuAbout/menuAbout';
+
+function Background () {
+  return (
+    <div className="background">
+      <MenuAbout />
+    </div>
+  )
+}
+
+export default Background;
---
+++ jogo-da-velha/src/components/menuAbout/menuAbout.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.css';
+
+const MenuAbout = () => {
+  return (
+    <p className="menu-about">About</p>
+  )
+}
+
+export default MenuAbout;
