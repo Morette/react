4fe09ab25dd4491e65425fdb5975133b43bf36b9
Thiago Morette <thiagomorette@gmail.com>
2019-08-05 17:08:38 -0300

    folder architecture started.. files removed to standardization

--- jogo-da-velha/src/App.js
+++ jogo-da-velha/src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import MenuAbout from "./components/menuAbout/menuAbout";
-import Background from './components/background';
 import "./App.css"
 
 function App() {
   return (
     <div className="App">
-      <Background />
+      <MenuAbout />
     </div>
   );
 }
--- jogo-da-velha/src/index.js
+++ jogo-da-velha/src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './styles/generic/reset.css';
+import './styles/elements/base.css';
+import './styles/settings/colors.css'
 
 ReactDOM.render(<App />, document.getElementById('root'));
