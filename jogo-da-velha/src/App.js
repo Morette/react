da416a8f9b9d08361c25b5ed4750b39e2139f282
Thiago Morette <thiagomorette@gmail.com>
2019-08-09 11:46:31 -0300

    created Hashtag component

--- jogo-da-velha/src/App.js
+++ jogo-da-velha/src/App.js
@@ -1,17 +1,12 @@
 import React from "react";
-import Header from './components/header'
-
-import Card from './objects/Card';
-import Player from './objects/Player'
+import Header from "./components/header"
+import Hashtag from "./components/Hashtag";
 
 function App() {
   return (
     <>
       <Header />
-      <Card>
-        <Player player="o"/>
-      </Card>
-      
+      <Hashtag />
     </>
   );
 }
