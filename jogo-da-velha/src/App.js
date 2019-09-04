97b353ef88ec50c5732393b85822d68069779591
Thiago Morette <thiagomorette@gmail.com>
2019-09-04 18:20:54 -0300

    create HeaderInternal component

--- jogo-da-velha/src/App.js
+++ jogo-da-velha/src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 import "./App.css"
 
+import About from "./objects/About";
 import Header from "./components/Header";
 import Hashtag from "./components/Hashtag";
 import Checkbox from "./objects/Checkbox";
-import About from "./objects/About";
-import MenuAbout from "./objects/MenuAbout";
-import IconClose from "./objects/IconClose";
-import LogoCollab from "./objects/LogoCollab";
+import HeaderInternal from "./components/HeaderInternal";
 
 const App = () => (
   <main className="app">
@@ -16,9 +14,7 @@ const App = () => (
     <Checkbox id="show" content="Mostrar Eventos" value="Mostrar Eventos" />
 
     <About>
-      <LogoCollab light />
-      <MenuAbout className="-light" />
-      <IconClose />
+      <HeaderInternal />
     </About>
   </main>
 );
---
+++ jogo-da-velha/src/components/HeaderInternal/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import MenuAbout from "../../objects/MenuAbout";
+import IconClose from "../../objects/IconClose";
+import LogoCollab from "../../objects/LogoCollab";
+
+const HeaderInternal = () => (
+  <header className="header-internal">
+    <LogoCollab light />
+    <MenuAbout className="-light" />
+    <IconClose />
+  </header>
+)
+
+export default HeaderInternal;
