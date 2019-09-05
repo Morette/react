f4f6a9f77128b761fd7eba85395c25d9cd634135
Thiago Morette <thiagomorette@gmail.com>
2019-09-05 09:41:20 -0300

    applied css to About object

--- jogo-da-velha/src/components/ProfileUser/index.js
+++ jogo-da-velha/src/components/ProfileUser/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 
+import './style.css';
 import Avatar from '../../images/avatar.png';
 
 import AvatarProfile from '../../objects/AvatarProfile';
 
 const ProfileUser = () => (
-  <dl>
-    <dd>
+  <dl className="profile-user">
+    <dd className="avatar">
       <AvatarProfile src={Avatar} alt={"Avatar Image"} width={130} />
     </dd>
-    <dt>Thiago Morette</dt>
-    <dd>Graduate engineer and passionate developer. Left engineering to attend my real passion developing softwares.</dd>
+    <dt className="name">Thiago Morette</dt>
+    <dd className="description">Graduate engineer and passionate developer. Left engineering to attend my real passion developing softwares.</dd>
   </dl>
 );
 
